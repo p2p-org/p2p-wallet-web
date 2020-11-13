81d374f08ee1c3ca2db57a3f8417fcb7e6fb2214
Nick Shtefan <nick.shtefan@gmail.com>
2020-11-13 18:55:47 +0300

    Updates

--- AddCoinModal.4b79d9b5.js
+++ AddCoinModal.4b79d9b5.js
@@ -852,7 +852,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53892" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57981" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
