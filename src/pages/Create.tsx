4fbcd465122e777a5b81a74a00c365f3feec1444
Nick Shtefan <nick.shtefan@gmail.com>
2020-11-14 20:07:23 +0300

    update create UI with password

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -7,11 +7,17 @@ import { styled } from 'linaria/react';
 
 import { createAccount } from 'store/actions/complex/blockchain';
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
+import { Button, Input, Icon } from 'components/ui';
+import { Header } from '../components/common/Header';
+
+
+
+
+const Wrapper = styled.div``;
+
+const Box = styled.div`
+  margin: auto;
+  max-width: 364px;
 `;
 
 const Form = styled.form`
@@ -19,16 +25,73 @@ const Form = styled.form`
   flex-direction: column;
 `;
 
+const Title = styled.div`
+  color: #000;
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 34px;
+  text-align: center;
+  margin-top: 100px;
+  margin-bottom: 16px;
+  `;
+
+const Text = styled.div`
+color: #000;
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 20px;
+text-align: center;
+margin-bottom: 8px;
+
+  `;
+
 const LinkStyled = styled(Link)`
   display: flex;
   justify-content: center;
-  margin-top: 20px;
+  margin-top: 56px;
+  color: #000;
+  text-decoration: none;
 `;
+
+const EyeIcon = styled(Icon)`
+  width: 24px;
+  height: 24px;
+  opacity: 0.5;
+`;
+
+const CreateButton = styled(Button)`
+  width: 100%;
+  height: 56px;
+  font-weight: 500;
+  color: #fff;
+  background: #000; 
+  line-height: 17px;
+  size: 14px;
+  font-style: normal;
+  margin-top: 32px;
+`;
+
+const SubTitle = styled.div`
+  color: #000;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 17px;
+  text-align: left;
+  margin-bottom: 12px;
+  margin-top: 24px;
+  opacity: 0.5;
+  `;
 
 export const Create: FunctionComponent = () => {
   const history = useHistory();
   const dispatch = useDispatch();
-  const [password, setPassword] = useState(bip39.generateMnemonic());
+  //const [password, setPassword] = useState(bip39.generateMnemonic());
+  const [error, setError] = useState(false);
+  const [password, setPassword] = useState(
+    '',
+  );
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -47,14 +110,43 @@ export const Create: FunctionComponent = () => {
     setPassword(e.target.value);
   };
 
+  const handleVisibility = () => {
+    console.log("Changing Visibility");
+  }
+
   return (
     <Wrapper>
+    <Header />
+    <Box>
+
       <Form onSubmit={handleSubmit}>
-        <h1>Create Wallet</h1>
-        <input name="password" value={password} onChange={handleChange} />
-        <button type="submit">Submit</button>
-        <LinkStyled to="/access">Access Wallet</LinkStyled>
+        <Title>Create Password</Title>
+        <Text>Create password to protect your wallet. It’s will be used for security actions confirmation and for editing your profile info.</Text>
+        <SubTitle>Create password</SubTitle>
+        <Input name="password" 
+                  type="password" 
+                  value={password} 
+                  onChange={handleChange}
+                  postfix={
+                    <EyeIcon name="eye" onclick={handleVisibility} />
+                  }
+          />
+          <SubTitle>Confirm password</SubTitle>
+          <Input name="password" 
+                  type="password" 
+                  value={password} 
+                  onChange={handleChange}
+                  postfix={
+                    <EyeIcon name="eye" onclick={handleVisibility} />
+                  }
+          />
+          <CreateButton type="submit" disabled={error}>
+             Continue
+           </CreateButton>
+
+        <LinkStyled to="/access">Already have wallet? Access it</LinkStyled>
       </Form>
+      </Box>
     </Wrapper>
   );
 };
