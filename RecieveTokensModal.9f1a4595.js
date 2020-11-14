93a46ebea9019d0639dbfb36c70f858c0d0e13f1
Nick Shtefan <nick.shtefan@gmail.com>
2020-11-14 22:37:29 +0300

    Updates

--- RecieveTokensModal.9f1a4595.js
+++ RecieveTokensModal.9f1a4595.js
@@ -213,7 +213,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60335" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49235" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
