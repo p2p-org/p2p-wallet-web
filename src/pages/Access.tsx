144b0197f53ee5d2a0edb2ef040230dac1daabc5
Nick Shtefan <nick.shtefan@gmail.com>
2020-11-14 19:05:08 +0300

    Update Access UI

--- src/pages/Access.tsx
+++ src/pages/Access.tsx
@@ -7,13 +7,17 @@ import { styled } from 'linaria/react';
 import throttle from 'lodash.throttle';
 
 import { createAccount, accessAccount } from 'store/actions/complex/blockchain';
+import { Header } from '../components/common/Header';
+import { Button, Input, Icon } from 'components/ui';
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-`;
+// const Wrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   height: 100vh;
+// `;
+
+const Wrapper = styled.div``;
 
 const Form = styled.form`
   display: flex;
@@ -26,15 +30,73 @@ const LinkStyled = styled(Link)`
   margin-top: 20px;
 `;
 
+const Title = styled.div`
+  color: #000;
+  font-weight: 500;
+  font-size: 27px;
+  line-height: 120%;
+  text-align: center;
+  margin-top: 100px;
+  margin-bottom: 32px;
+  `;
+
+const SubTitle = styled.div`
+  color: #000;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 17px;
+  text-align: left;
+  margin-bottom: 12px;
+  opacity: 0.5;
+  `;
+
+const CreateButton = styled(Button)`
+  width: 100%;
+  height: 56px;
+  font-weight: 500;
+  color: #fff;
+  background: #000; 
+  line-height: 17px;
+  size: 14px;
+  font-style: normal;
+  margin-top: 32px;
+`;
+
+const Box = styled.div`
+  margin: auto;
+  max-width: 364px;
+`;
+
+const InputSeed = styled.textarea`
+  height: 72px;
+  margin-bottom: 24px;
+  outline: none !important;
+  border:1px solid #D2D2D2;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 15px;
+  padding: 16px;
+  `;
+
+const EyeIcon = styled(Icon)`
+  width: 24px;
+  height: 24px;
+  opacity: 0.5;
+`;
+
 export const Access: FunctionComponent = () => {
   const history = useHistory();
   const dispatch = useDispatch();
   const [error, setError] = useState(false);
-  const [password, setPassword] = useState(
+  const [mnemonic, setMnemonic] = useState(
     'galaxy lend nose glow equip student way hockey step dismiss expect silent',
   );
+  const [password, setPassword] = useState(
+    '',
+  );
 
-  const validatePassword = useCallback(
+  const validateMnemonic = useCallback(
     throttle(
       (nextPassword: string) => {
         if (bip39.validateMnemonic(nextPassword)) {
@@ -51,34 +113,59 @@ export const Access: FunctionComponent = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (password.length === 0) {
+    if (mnemonic.length === 0) {
       return;
     }
-
-    dispatch(accessAccount(password));
-
+    dispatch(accessAccount(mnemonic));
     setTimeout(() => {
       history.push('/wallets');
     }, 10);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const nextMnemonic: string = e.target.value.trim();
+
+    validateMnemonic(nextMnemonic);
+    setMnemonic(nextMnemonic);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const nextPassword: string = e.target.value.trim();
 
-    validatePassword(nextPassword);
     setPassword(nextPassword);
   };
 
+  const handleVisibility = () => {
+    console.log("Changing Visibility");
+
+  }
+
   return (
     <Wrapper>
-      <Form onSubmit={handleSubmit}>
-        <h1>Access Wallet</h1>
-        <input name="password" value={password} onChange={handleChange} />
-        <button type="submit" disabled={error}>
-          Submit
-        </button>
-        <LinkStyled to="/create">Create Wallet</LinkStyled>
+       <Header />
+       <Box>
+         <Form onSubmit={handleSubmit}>
+
+           <Title>Access Wallet</Title>
+
+           <SubTitle>Enter your Seed, to get access to wallet </SubTitle>
+           <InputSeed name="mnemonic" value={mnemonic} onChange={handleChange} />
+
+           <SubTitle>Enter Password (optional) </SubTitle>
+           <Input name="password" 
+                  type="password" 
+                  value={password} 
+                  onChange={handlePasswordChange}
+                  postfix={
+                    <EyeIcon name="eye" onclick={handleVisibility} />
+                  }
+          />
+          
+           <CreateButton type="submit" disabled={error}>
+             Continue
+           </CreateButton>
       </Form>
+      </Box>
     </Wrapper>
   );
 };
