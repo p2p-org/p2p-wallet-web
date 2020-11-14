f68040d513b32af3dd16fc6543d5356397cd86c6
Alexey Elizarov <felex-ae@ya.ru>
2020-11-14 08:12:32 +0300

    Updates

--- TransactionDetailsModal.ebfc6aa9.js
+++ TransactionDetailsModal.ebfc6aa9.js
@@ -279,7 +279,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57043" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59253" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
