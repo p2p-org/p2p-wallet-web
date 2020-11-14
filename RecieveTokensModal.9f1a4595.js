7d5f3facbf31e62a83bb84334f409d7930d07952
Alexey Elizarov <felex-ae@ya.ru>
2020-11-14 05:03:55 +0300

    Updates

--- RecieveTokensModal.9f1a4595.js
+++ RecieveTokensModal.9f1a4595.js
@@ -213,7 +213,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51984" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49667" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
