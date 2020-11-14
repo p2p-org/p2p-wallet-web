52c096f19f9f8cf94b1ed510053cd24aca84a186
Alexey Elizarov <felex-ae@ya.ru>
2020-11-15 02:19:36 +0300

    fix: retry fetch transaction details, refactoring send

---
+++ src/components/pages/swap/SwapWidget/SwapWidget.tsx
@@ -0,0 +1,73 @@
+import React, { FunctionComponent, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+
+import * as web3 from '@solana/web3.js';
+
+import { SendSwapWidget } from 'components/common/SendSwapWidget';
+import { transferTokens } from 'store/actions/complex';
+
+type Props = {
+  publicKey: string;
+};
+
+export const SendWidget: FunctionComponent<Props> = ({ publicKey = '' }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [fromTokenPublicKey, setFromTokenPublicKey] = useState(publicKey);
+  const [fromTokenAmount, setFromTokenAmount] = useState('');
+  const [toTokenPublicKey, setToTokenPublicKey] = useState('');
+
+  const handleBackClick = () => {
+    history.replace('/wallets');
+  };
+
+  const handleToPublicKeyChange = (nextPublicKey: string) => {
+    setToTokenPublicKey(nextPublicKey);
+  };
+
+  const handleSubmit = async () => {
+    const amount = Math.round(Number.parseFloat(fromTokenAmount) * 10 ** 9);
+
+    if (!amount || amount <= 0) {
+      throw new Error('Invalid amount');
+    }
+
+    try {
+      const signature = await dispatch(
+        transferTokens({
+          sourcePublicKey: new web3.PublicKey(fromTokenPublicKey),
+          destPublicKey: new web3.PublicKey(toTokenPublicKey),
+          amount,
+        }),
+      );
+
+      history.push(`/send/${publicKey}/result`, { signature });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const handleTokenChange = (nextTokenPublicKey: string) => {
+    setFromTokenPublicKey(nextTokenPublicKey);
+  };
+
+  const handleAmountChange = (nextTokenAmount: string) => {
+    setFromTokenAmount(nextTokenAmount);
+  };
+
+  return (
+    <SendSwapWidget
+      title="Send tokens"
+      actionText="Send"
+      fromTokenPublicKey={fromTokenPublicKey}
+      fromTokenAmount={fromTokenAmount}
+      toTokenPublicKey={toTokenPublicKey}
+      onTokenChange={handleTokenChange}
+      onAmountChange={handleAmountChange}
+      onToPublicKeyChange={handleToPublicKeyChange}
+      onBackClick={handleBackClick}
+      onSubmit={handleSubmit}
+    />
+  );
+};
