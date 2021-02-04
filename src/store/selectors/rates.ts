d9b90cca2c8810a7284e2457cd307a30dcaac60e
Alexey Elizarov <felex-ae@ya.ru>
2021-02-04 12:46:57 +0300

    refactor: rateSelector if symbol not specified return

--- src/store/selectors/rates.ts
+++ src/store/selectors/rates.ts
@@ -1,11 +1,11 @@
 import { RootState } from 'store/rootReducer';
 
 export const rateSelector = (symbol?: string) => (state: RootState) => {
-  const rate = state.rate.markets[`${symbol}/USDT`];
-
   if (!symbol) {
     return null;
   }
+
+  const rate = state.rate.markets[`${symbol}/USDT`];
 
   if (['USDT', 'USDC'].includes(symbol)) {
     return rate || 1;
