1c8e6dcfa3527f21f1f2907dc4b7ab5b6e9e302d
Alexey Elizarov <felex-ae@ya.ru>
2021-06-08 18:28:15 +0300

    feat: sort wallets by usd balance

--- src/components/pages/wallets/TokensWidget/TokenList/TokenList.tsx
+++ src/components/pages/wallets/TokensWidget/TokenList/TokenList.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
 
 import { styled } from '@linaria/react';
 
@@ -10,6 +11,26 @@ import { TokenRow } from '../TokenRow';
 const Wrapper = styled.div`
   margin: 0 10px;
 `;
+
+const sortByUSDBalance = (rates: { [pair: string]: number }) => (
+  a: TokenAccount,
+  b: TokenAccount,
+) => {
+  const aUSDBalance = a.balance.toNumber() * (a.mint.symbol ? rates[a.mint.symbol] : 1);
+  const bUSDBalance = b.balance.toNumber() * (b.mint.symbol ? rates[b.mint.symbol] : 1);
+
+  if (aUSDBalance < bUSDBalance) {
+    return 1;
+  }
+
+  if (aUSDBalance === bUSDBalance) {
+    if (a.balance.lt(b.balance)) {
+      return 1;
+    }
+  }
+
+  return -1;
+};
 
 type Props = {
   items: TokenAccount[];
@@ -24,23 +45,25 @@ export const TokenList: FunctionComponent<Props> = ({
   isZeroBalancesHidden = true,
   isHidden = false,
 }) => {
-  if (items.length === 0 && !isHidden) {
+  const rates = useSelector((state) => state.rate.markets);
+
+  const tokens = items.sort(sortByUSDBalance(rates));
+
+  if (tokens.length === 0 && !isHidden) {
     return <LoaderBlock />;
   }
 
   return (
     <Wrapper>
-      {items
-        .sort((a, b) => b.balance.cmp(a.balance))
-        .map((item) => (
-          <TokenRow
-            key={item.address.toBase58()}
-            token={item}
-            isSelected={item.mint.symbol === selectedSymbol}
-            isHidden={isHidden}
-            isZeroBalancesHidden={isZeroBalancesHidden}
-          />
-        ))}
+      {tokens.map((item) => (
+        <TokenRow
+          key={item.address.toBase58()}
+          token={item}
+          isSelected={item.mint.symbol === selectedSymbol}
+          isHidden={isHidden}
+          isZeroBalancesHidden={isZeroBalancesHidden}
+        />
+      ))}
     </Wrapper>
   );
 };
