57a20e62c5d718e506b0c07d29fb07df8c732be4
Alexey Elizarov <felex-ae@ya.ru>
2021-06-17 11:53:28 +0300

    feat: babel source maps

--- config-overrides.js
+++ config-overrides.js
@@ -5,16 +5,47 @@ const {
   addWebpackModuleRule,
   addWebpackPlugin,
   setWebpackPublicPath,
+  disableChunk,
+  getBabelLoader,
 } = require('customize-cra');
 const SpritePlugin = require('svg-sprite-loader/plugin');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 
+const addCustomBabelLoader = () => (config) => {
+  const babelLoader = getBabelLoader(config);
+
+  delete babelLoader.options.babelrc;
+  delete babelLoader.options.configFile;
+  delete babelLoader.options.cacheIdentifier;
+
+  babelLoader.use = [
+    {
+      loader: babelLoader.loader,
+      options: babelLoader.options,
+    },
+    {
+      loader: '@linaria/webpack-loader',
+      options: {
+        cacheDirectory: 'src/.linaria_cache',
+        sourceMap: isDev,
+      },
+    },
+  ];
+
+  delete babelLoader.loader;
+  delete babelLoader.options;
+  delete babelLoader.include;
+
+  return config;
+};
+
 module.exports = override(
   useBabelRc(),
   setWebpackPublicPath(process.env.REACT_APP_BASENAME || '/'),
   addWebpackPlugin(new SpritePlugin()),
+  isDev && disableChunk(),
   !isDev &&
     addWebpackPlugin(
       new PrerenderSPAPlugin({
@@ -22,20 +53,7 @@ module.exports = override(
         staticDir: path.join(__dirname, 'build'),
       }),
     ),
-  addWebpackModuleRule({
-    test: /\.tsx?$/,
-    exclude: /node_modules/,
-    use: [
-      { loader: 'babel-loader' },
-      {
-        loader: '@linaria/webpack-loader',
-        options: {
-          cacheDirectory: 'src/.linaria_cache',
-          sourceMap: isDev,
-        },
-      },
-    ],
-  }),
+  addCustomBabelLoader(),
   addWebpackModuleRule({
     test: /-icon\.svg$/,
     use: [
