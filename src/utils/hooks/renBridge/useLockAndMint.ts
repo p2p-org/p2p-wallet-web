04e99d88135944f8a5aef9f573fc7b359f20de40
Alexey Elizarov <felex-ae@ya.ru>
2021-08-17 15:23:06 +0300

    fix: typescript

--- src/utils/hooks/renBridge/useLockAndMint.ts
+++ src/utils/hooks/renBridge/useLockAndMint.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any, no-unused-vars, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any, no-unused-vars, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unused-vars,react-hooks/rules-of-hooks */
 
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
