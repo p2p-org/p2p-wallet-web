db5a2e138bba7253a00a434d7e48c0a74b1b91d2
Alexey Elizarov <felex-ae@ya.ru>
2021-08-21 16:25:05 +0300

    chore: bump token registry

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-private-methods', { loose: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     [
       'module-resolver',
       {
