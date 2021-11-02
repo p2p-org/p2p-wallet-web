e3b2c91755f1b03fb78f3abaa7cc14eec7e7c7d5
Alexey Elizarov <felex-ae@ya.ru>
2021-11-02 09:24:27 +0200

    fix: links

--- src/components/pages/landing/common/Button/Button.tsx
+++ src/components/pages/landing/common/Button/Button.tsx
@@ -66,6 +66,66 @@ export const ButtonLink = styled(Link)`
   }
 `;
 
+export const ButtonA = styled.a`
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 226px;
+  height: 61px;
+  padding: 0 24px;
+
+  color: #000;
+  font-weight: 500;
+  font-size: 18px;
+  font-family: 'Aktiv Grotesk Corp', sans-serif;
+  line-height: 140%;
+  text-decoration: none;
+
+  background-color: #fff;
+  border-radius: 32px;
+  cursor: pointer;
+
+  transition: background-color 0.1s;
+
+  &:hover {
+    background-color: #71e6ff;
+  }
+
+  &.green {
+    background-color: #bcff4e;
+
+    &:hover {
+      background-color: #e3ff74;
+    }
+  }
+
+  &.glow {
+    &::before {
+      position: absolute;
+      right: 10px;
+      bottom: 0;
+      left: 10px;
+
+      height: 50px;
+
+      background: linear-gradient(90deg, #fff 0%, #fff 100%);
+      border-radius: 32px;
+      opacity: 0.3;
+      filter: blur(30px);
+
+      content: '';
+    }
+
+    &.green {
+      &::before {
+        background: linear-gradient(90deg, #bcff4e 0%, #bcff4e 100%);
+      }
+    }
+  }
+`;
+
 const Text = styled.div`
   &.web,
   &.ios {
@@ -109,12 +169,13 @@ export const ButtonIOS: FC<ButtonProps & HTMLAttributes<HTMLElement>> = ({
   ...props
 }) => {
   return (
-    <ButtonLink
-      to={appStorePath}
+    <ButtonA
+      href={appStorePath}
+      target="_blank"
       className={classNames(className, { glow, green: true })}
       {...props}>
       <Text className="ios">Download for iOS</Text>
-    </ButtonLink>
+    </ButtonA>
   );
 };
 
@@ -124,11 +185,12 @@ export const ButtonAndroid: FC<ButtonProps & HTMLAttributes<HTMLElement>> = ({
   ...props
 }) => {
   return (
-    <ButtonLink
-      to={playStorePath}
+    <ButtonA
+      href={playStorePath}
+      target="_blank"
       className={classNames(className, { glow, green: true })}
       {...props}>
       <Text className="android">Download for Android</Text>
-    </ButtonLink>
+    </ButtonA>
   );
 };
