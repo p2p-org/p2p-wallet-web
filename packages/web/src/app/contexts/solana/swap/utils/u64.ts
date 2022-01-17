1f9f17d37bd23ded6d4c5c46cd7bae14067750c7
Alexey Elizarov <felex-ae@ya.ru>
2022-01-17 12:09:58 +0300

    feat(sail): sync debug, ability to pass fee payer to tx

--- packages/web/src/app/contexts/solana/swap/utils/u64.ts
+++
@@ -1,7 +0,0 @@
-import { u64 } from '@solana/spl-token';
-
-// @blockchain/spl-token v0.0.13 has a bug where
-// u64.fromBuffer returns `BN` rather than `u64`
-export function u64FromBuffer(buffer: Buffer): u64 {
-  return new u64(u64.fromBuffer(buffer).toString());
-}
