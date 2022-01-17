9fbdf71cb23de3f708cc3979875f9b4e68a46d82
Alexey Elizarov <felex-ae@ya.ru>
2022-01-17 14:16:57 +0300

    fix: webpack build mjs example

--- packages/example/craco.config.js
+++ packages/example/craco.config.js
@@ -3,7 +3,8 @@ const {
   loaderByName,
   getLoaders,
   removeLoaders,
-  throwUnexpectedConfigError, addBeforeLoader,
+  throwUnexpectedConfigError,
+  addBeforeLoader,
 } = require('@craco/craco');
 
 const throwError = (message) =>
@@ -14,35 +15,58 @@ const throwError = (message) =>
     githubIssueQuery: 'webpack',
   });
 
+function addBabelLoader(config, isDev) {
+  console.log('adding ts babel-loader');
+
+  const babelLoader = {
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: [
+      { loader: 'babel-loader' },
+      {
+        loader: '@linaria/webpack-loader',
+        options: {
+          cacheDirectory: 'src/.linaria_cache',
+          sourceMap: isDev,
+        },
+      },
+    ],
+  };
+
+  const { isAdded: babelLoaderIsAdded } = addBeforeLoader(
+    config,
+    loaderByName('babel-loader'),
+    babelLoader,
+  );
+  if (!babelLoaderIsAdded) throwError('failed to add babel-loader');
+  console.log('added babel-loader');
+}
+
+function addMjsLoader(config, isDev) {
+  console.log('adding mjs loader');
+
+  const mjsLoader = {
+    test: /\.mjs$/,
+    include: /node_modules/,
+    type: 'javascript/auto',
+  };
+
+  const { isAdded: mjsLoaderIsAdded } = addAfterLoader(
+    config,
+    loaderByName('babel-loader'),
+    mjsLoader,
+  );
+  if (!mjsLoaderIsAdded) throwError('failed to add mjs loader');
+  console.log('added mjs loader');
+}
+
 module.exports = {
   webpack: {
     configure: (config, { env }) => {
       const isDev = env === 'development';
 
-      console.log('adding ts babel-loader');
-
-      const babelLoader = {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: 'babel-loader' },
-          {
-            loader: '@linaria/webpack-loader',
-            options: {
-              cacheDirectory: 'src/.linaria_cache',
-              sourceMap: isDev,
-            },
-          },
-        ],
-      };
-
-      const { isAdded: babelLoaderIsAdded } = addBeforeLoader(
-        config,
-        loaderByName('babel-loader'),
-        babelLoader,
-      );
-      if (!babelLoaderIsAdded) throwError('failed to add babel-loader');
-      console.log('added babel-loader');
+      addBabelLoader(config, isDev);
+      addMjsLoader(config, isDev);
 
       return config;
     },
