8bf4caee79436b0b3040fd657c4d908bd02b2f9f
Грозных Артем Сергеевич <ext.agroznykh@tinkoff.ru>
2022-03-28 14:52:05 +0300

    add eslint curly rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
-"use strict";
+'use strict';
 
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ["@p2p-wallet-web/eslint-config-react"],
-  ignorePatterns: ["*.js"],
+  extends: ['@p2p-wallet-web/eslint-config-react'],
+  ignorePatterns: ['*.js'],
   parserOptions: {
-    project: "tsconfig.json",
-  }
+    project: 'tsconfig.json',
+  },
+  rules: {
+    curly: 'error',
+  },
 };
