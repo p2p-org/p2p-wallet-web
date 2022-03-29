6ec9e9dce6b049e2f580d0297e3db28dceec7eb2
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-03-29 18:17:27 +0300

    chore: remove tsc from linting

--- packages/web/lint-staged.config.js
+++ packages/web/lint-staged.config.js
@@ -3,7 +3,4 @@ module.exports = {
         "pretty-quick --staged",
         "eslint"
     ],
-    "*.{tsx,ts}": [
-        "bash -c tsc --noEmit --emitDeclarationOnly false --project ./tsconfig.json"
-    ]
 }
