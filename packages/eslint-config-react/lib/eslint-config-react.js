a4f29faa271a0fcdf99e730994822850e570a50e
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-03-29 18:46:45 +0300

    chore: add recommended @typescript/eslint rules

--- packages/eslint-config-react/lib/eslint-config-react.js
+++ packages/eslint-config-react/lib/eslint-config-react.js
@@ -1,22 +1,47 @@
-"use strict";
+'use strict';
 
 module.exports = {
   env: {
     browser: true,
   },
   extends: [
-    "@p2p-wallet-web",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
+    '@p2p-wallet-web',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
-  settings: { react: { version: "detect" } },
+  settings: { react: { version: 'detect' } },
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/no-unescaped-entities": "off",
-    "react/prop-types": "off",
-    "react/display-name": "off",
-    "@typescript-eslint/no-explicit-any": 'error',
-    "@typescript-eslint/no-unused-vars": "error"
+    "require-await": "off",
+    "no-magic-numbers": "off",
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-shadow': 'error',
+    "@typescript-eslint/require-await": "error",
+    "@typescript-eslint/await-thenable": "error",
+    "@typescript-eslint/no-non-null-assertion": "error",
+    "@typescript-eslint/no-for-in-array": "error",
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
+    "@typescript-eslint/prefer-as-const": "error",
+    "@typescript-eslint/no-empty-interface": [
+      "error",
+      {
+        "allowSingleExtends": false
+      }
+    ],
+    "@typescript-eslint/no-magic-numbers": [
+      "error",
+      {
+        ignore: [-1, 0, 1],
+        ignoreEnums: true,
+        ignoreNumericLiteralTypes: true,
+        ignoreArrayIndexes: true
+      }
+    ]
   },
 };
