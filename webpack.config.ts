0a683a3763b7cc35ad38fc79bf8848157fd95deb
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-04-05 18:47:21 +0300

    chore(web): initial devserver setup

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,57 @@
+import path from 'path';
 import type { Configuration as WebpackConfiguration } from 'webpack';
 import type { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 
-interface Configuration extends WebpackConfiguration {
+interface Configuration extends WebpackConfiguration, WebpackDevServerConfiguration {
   devServer?: WebpackDevServerConfiguration;
 }
 
-const config: Configuration = {};
+type ConfigFn = (env: any, argv: any) => Configuration;
 
-export default config;
+const config: ConfigFn = (env, argv) => {
+  console.log(env);
+  console.log(argv);
+  return {
+    mode: 'development',
+
+    entry: './packages/web/src/index.tsx',
+
+    output: {
+      publicPath: '/',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+            },
+          },
+        },
+      ],
+    },
+
+    devServer: {
+      client: {
+        overlay: {
+          errors: true,
+          warnings: false,
+        }, // @TODO check
+        progress: true,
+      },
+      static: {
+        directory: path.join(__dirname, '/packages/web/public'),
+      },
+      historyApiFallback: true, // @TODO check
+      compress: true,
+      port: 9000,
+      // hot: true // @TODO check
+    },
+  };
+};
+
+module.exports = config;
