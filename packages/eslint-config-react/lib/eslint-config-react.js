331358512b87cde438c0a6d532e14b1d658a2431
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-04-05 17:48:28 +0300

    fix(web): mover general eslint rules to the main config

--- packages/eslint-config-react/lib/eslint-config-react.js
+++ packages/eslint-config-react/lib/eslint-config-react.js
@@ -12,36 +12,9 @@ module.exports = {
   ],
   settings: { react: { version: 'detect' } },
   rules: {
-    "require-await": "off",
-    "no-magic-numbers": "off",
-    'no-console': ['error', { allow: ['warn', 'error'] }],
     'react/react-in-jsx-scope': 'off',
     'react/no-unescaped-entities': 'off',
     'react/prop-types': 'off',
     'react/display-name': 'off',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-shadow': 'error',
-    "@typescript-eslint/require-await": "error",
-    "@typescript-eslint/await-thenable": "error",
-    "@typescript-eslint/no-non-null-assertion": "error",
-    "@typescript-eslint/no-for-in-array": "error",
-    "@typescript-eslint/no-unnecessary-type-assertion": "error",
-    "@typescript-eslint/prefer-as-const": "error",
-    "@typescript-eslint/no-empty-interface": [
-      "error",
-      {
-        "allowSingleExtends": false
-      }
-    ],
-    "@typescript-eslint/no-magic-numbers": [
-      "error",
-      {
-        ignore: [-1, 0, 1],
-        ignoreEnums: true,
-        ignoreNumericLiteralTypes: true,
-        ignoreArrayIndexes: true
-      }
-    ]
-  },
+  }
 };
