bf6bb86cdcc75b78bd670793f3dc58e8fd40d50e
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-04-06 18:18:25 +0300

    chore(web): add env vars

--- webpack.config.ts
+++ webpack.config.ts
@@ -8,11 +8,10 @@ interface Configuration extends WebpackConfiguration, WebpackDevServerConfigurat
 
 type ConfigFn = (env: any, argv: any) => Configuration;
 
+// @ts-ignore
 const config: ConfigFn = (env, argv) => {
-  console.log(env);
-  console.log(argv);
   return {
-    mode: 'development',
+    mode: argv.mode,
 
     entry: './packages/web/src/index.tsx',
 
@@ -28,7 +27,34 @@ const config: ConfigFn = (env, argv) => {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+              presets: [
+                ['react-app', { flow: false, typescript: true, runtime: 'automatic' }],
+                '@linaria',
+              ],
+              plugins: [
+                ['@babel/plugin-proposal-private-methods', { loose: true }],
+                ['@babel/plugin-proposal-class-properties', { loose: true }],
+                ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+                [
+                  'module-resolver',
+                  {
+                    root: ['./packages/web/src'],
+                    alias: {
+                      constants: './packages/web/src/constants',
+                      config: './packages/web/src/config',
+                      utils: './packages/web/src/utils',
+                      lib: './packages/web/src/lib',
+                      store: './packages/web/src/store',
+                      api: './packages/web/src/api',
+                      app: './packages/web/src/app',
+                      pages: './packages/web/src/pages',
+                      components: './packages/web/src/components',
+                      assets: './packages/web/src/assets',
+                      styles: './packages/web/src/styles',
+                    },
+                  },
+                ],
+              ],
             },
           },
         },
@@ -51,6 +77,8 @@ const config: ConfigFn = (env, argv) => {
       port: 9000,
       // hot: true // @TODO check
     },
+
+    plugins: [],
   };
 };
 
