f456506079e96815f661680d6a73e118d1cdd891
agroznykh <artem.groznykh@p2p.org>
2022-04-11 13:44:15 +0300

    feat(web): commit

---
+++ packages/web/src/components/pages/swap/SwapSlippageWidget/SlippageInput/SlippageInput.tsx
@@ -0,0 +1,95 @@
+import type { FC } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { styled } from '@linaria/react';
+import { theme } from '@p2p-wallet-web/ui';
+
+const Wrapper = styled.div`
+  display: flex;
+
+  color: ${theme.colors.textIcon.primary};
+`;
+
+const Postfix = styled.span``;
+
+const InputWrapper = styled.span``;
+
+const InputElement = styled.input`
+  width: 100%;
+
+  font-weight: 500;
+  font-size: 16px;
+  font-style: normal;
+  letter-spacing: 0.01em;
+
+  border-width: 0;
+
+  &:focus,
+  &:active {
+    border-width: 0;
+  }
+`;
+
+type CustomProps = {
+  deafultValue: string;
+  onChangeValue: (value: string) => void;
+  maxSlippage: number;
+};
+
+type Props = React.InputHTMLAttributes<HTMLInputElement> & CustomProps;
+
+export const SlippageInput: FC<Props> = ({ deafultValue, maxSlippage, onChangeValue }) => {
+  const ref = useRef<HTMLInputElement>(null);
+  const [inputValue, setInputValue] = useState(deafultValue);
+
+  useEffect(() => {
+    ref.current && ref.current.focus();
+  }, [ref.current]);
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let newValue = e.target.value.replace(/,/g, '.'); // , to .
+
+    // // ability to enter dot
+    if (newValue === '.') {
+      newValue = '0.';
+    } else {
+      newValue = newValue
+        .replace(/[^.\d]/g, '')
+        .replace(/^0+(\d)/, '$1')
+        .replace(/^\./, '0.')
+        .replace(/(\d+\.\d{0,1})(\d+)?/, '$1');
+    }
+
+    setInputValue(newValue);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const newValue = Number(e.target.value);
+
+    onChangeValue(String(Math.min(newValue, maxSlippage)));
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (['Enter', 'Escape'].includes(e.key)) {
+      handleBlur(e as unknown as React.FocusEvent<HTMLInputElement>);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <InputWrapper>
+        <InputElement
+          ref={ref}
+          type="text"
+          inputMode="decimal"
+          placeholder="20"
+          value={inputValue}
+          onChange={handleValueChange}
+          onBlur={handleBlur}
+          onKeyPress={handleKeyPress}
+        />
+      </InputWrapper>
+      <Postfix>%</Postfix>
+    </Wrapper>
+  );
+};
