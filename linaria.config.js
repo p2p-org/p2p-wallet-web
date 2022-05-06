635654376341eb21124122de7ee561ec37e31329
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-05-06 17:19:45 +0300

    chore: move config files

--- linaria.config.js
+++
@@ -1,5 +0,0 @@
-// @FIXME should be in the same dir as webpackconfig
-module.exports = {
-  evaluate: true,
-  displayName: process.env.NODE_ENV !== 'production',
-};
---
+++ packages/configs/linaria.config.js
@@ -0,0 +1,5 @@
+// @FIXME should be in the same dir as webpackconfig
+module.exports = {
+  evaluate: true,
+  displayName: process.env.NODE_ENV !== 'production',
+};
