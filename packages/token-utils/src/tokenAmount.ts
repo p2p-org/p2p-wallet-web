efcdbeb29b615c2f8fa382f7b964b49b60c0e1f8
agroznykh <artem.groznykh@p2p.org>
2022-05-18 11:04:15 +0300

    feat(web): fix amounts formatting on Send page

--- packages/token-utils/src/tokenAmount.ts
+++ packages/token-utils/src/tokenAmount.ts
@@ -1,3 +1,4 @@
+import type { BigintIsh, Token } from '@saberhq/token-utils';
 import { TokenAmount as TokenAmountOriginal } from '@saberhq/token-utils';
 
 export const NUMBER_FORMAT = {
@@ -6,6 +7,20 @@ export const NUMBER_FORMAT = {
   groupSize: 3,
 };
 export class TokenAmount extends TokenAmountOriginal {
+  override new(token: Token, amount: BigintIsh): this {
+    return new TokenAmount(token, amount) as this;
+  }
+
+  static override parse(token: Token, uiAmount: string): TokenAmount {
+    const value = super.parse(token, uiAmount);
+    return new TokenAmount(token, value.raw);
+  }
+
+  override divideByInteger(other: BigintIsh): TokenAmount {
+    const value = super.divideByInteger(other);
+    return new TokenAmount(this.token, value.raw);
+  }
+
   override formatUnits(format = NUMBER_FORMAT) {
     return super.formatUnits(format);
   }
