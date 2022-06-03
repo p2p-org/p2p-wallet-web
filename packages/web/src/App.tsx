eb48d492e3ca7d1c6cf28795b3495f05862252a8
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-06-03 18:07:24 +0300

    chore: test GITHUB_TOKEN

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -27,6 +27,10 @@ dayjs.extend(localizedFormat);
 
 const App: React.FC = () => {
   const Router: React.ElementType = process.env.REACT_APP_STAGING ? HashRouter : BrowserRouter;
+  console.log(process.env.REACT_APP_STAGING);
+  console.log(process.env.REACT_APP_BASENAME);
+  console.log(process.env.PUBLIC_URL);
+  console.log(process.env.GITHUB_TOKEN);
 
   return (
     <>
