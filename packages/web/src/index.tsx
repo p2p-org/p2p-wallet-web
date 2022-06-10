c57efdd823e92b6e0871ddf14875234356e616d3
Artem Groznykh <artem.groznykh@p2p.org>
2022-06-10 12:39:09 +0300

    feat(web): base sentry integration

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -7,12 +7,29 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
 import { globalCss } from '@p2p-wallet-web/ui';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 
 import { initAmplitude } from 'utils/analytics';
 
 initAmplitude();
 
 export const global = globalCss;
+
+Sentry.init({
+  dsn: 'https://088e2b0cd1f44164a6ad584ec3fbab31@o1272413.ingest.sentry.io/6479116',
+  integrations: [new BrowserTracing()],
+  environment: 'dev',
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+  /*tracesSampler: function () {
+    const args = Array.from(arguments);
+    console.log('args -', args);
+    throw new Error('dont send error to sentry');
+  },*/
+});
 
 const render = () => {
   // Load the app dynamically, which allows for hot-reloading in development mode.
