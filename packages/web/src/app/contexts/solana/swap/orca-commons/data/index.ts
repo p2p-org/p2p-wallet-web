89943711096696ceac105436bc25ba6434fc8e06
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-06-14 11:26:43 +0300

    [PWN-3499] feat: cloudflare orca worker wallet integration

--- packages/web/src/app/contexts/solana/swap/orca-commons/data/index.ts
+++ packages/web/src/app/contexts/solana/swap/orca-commons/data/index.ts
@@ -1,11 +1,2 @@
-export { default as devnetPools } from './json/pools/devnet.json';
-export { default as mainnetPools } from './json/pools/mainnet.json';
-export { default as testnetPools } from './json/pools/testnet.json';
-export { default as devnetProgramIds } from './json/programIds/devnet.json';
-export { default as mainnetProgramIds } from './json/programIds/mainnet.json';
-export { default as testnetProgramIds } from './json/programIds/testnet.json';
-export { default as devnetTokens } from './json/tokens/devnet.json';
-export { default as mainnetTokens } from './json/tokens/mainnet.json';
-export { default as testnetTokens } from './json/tokens/testnet.json';
 export * from './Pool';
 export * from './Token';
