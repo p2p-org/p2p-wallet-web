fb6d28b9cd287754d5d8ae2611966c0eb673a9fa
Alexey Elizarov <felex-ae@ya.ru>
2022-08-18 18:38:02 +0300

    [PWN-3823] modals manager service

---
+++ packages/web/src/new/services/ModalService/ModalService.ts
@@ -0,0 +1,72 @@
+import { action, makeObservable, observable } from 'mobx';
+import { last } from 'ramda';
+import { singleton } from 'tsyringe';
+
+import type { ModalType } from './types';
+
+export type ModalState = { modalType: ModalType; modalId: number; props: any };
+
+export interface ModalServiceType {
+  openModal: <T, S extends {}>(modalType: ModalType, props?: S) => Promise<T | void>; // TODO: right return type
+  closeModal: <T>(modalId: number, result?: T) => T | void; // TODO: right return type
+  closeTopModal: () => void;
+}
+
+@singleton()
+export class ModalService implements ModalServiceType {
+  modals: ModalState[] = [];
+  private _modalIdCounter = 0;
+  private _promises = new Map();
+
+  constructor() {
+    makeObservable(this, {
+      modals: observable,
+
+      openModal: action,
+      closeModal: action,
+      closeTopModal: action,
+    });
+  }
+
+  openModal<T, S extends {}>(modalType: ModalType, props?: S): Promise<T | void> {
+    ++this._modalIdCounter;
+
+    this.modals.push({
+      modalType,
+      modalId: this._modalIdCounter,
+      props,
+    });
+
+    const promise = new Promise<T>((resolve) => {
+      this._promises.set(this._modalIdCounter, {
+        modalId: this._modalIdCounter,
+        resolve,
+      });
+    });
+
+    // @ts-ignore
+    promise.modalId = this._modalIdCounter;
+
+    return promise;
+  }
+
+  closeModal<T>(modalId: number, result?: T): T | void {
+    this.modals = this.modals.filter((modal) => modal.modalId !== modalId);
+
+    const dialogInfo = this._promises.get(modalId);
+    if (dialogInfo) {
+      dialogInfo.resolve(result);
+      this._promises.delete(modalId);
+    }
+
+    return result;
+  }
+
+  closeTopModal(): void {
+    if (this.modals.length === 0) {
+      return;
+    }
+
+    this.closeModal(last(this.modals)!.modalId);
+  }
+}
---
+++ packages/web/src/new/ui/modals/ModalManager/ModalManager.tsx
@@ -0,0 +1,159 @@
+import type { FC, LazyExoticComponent } from 'react';
+import { lazy, Suspense, useCallback } from 'react';
+import * as React from 'react';
+
+import { styled } from '@linaria/react';
+import { zIndexes } from '@p2p-wallet-web/ui';
+import classNames from 'classnames';
+import { observer } from 'mobx-react-lite';
+import { expr } from 'mobx-utils';
+
+import type { ModalPropsType } from 'app/contexts';
+import { ModalType } from 'app/contexts';
+import { ScrollFix } from 'components/common/ScollFix';
+import { useViewModel } from 'new/core/viewmodels/useViewModel';
+
+import { ModalManagerViewModel } from './ModalManager.ViewModel';
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: ${zIndexes.modal};
+
+  width: 100vw;
+  height: 100vh;
+
+  background-color: rgba(0, 0, 0, 0.6);
+
+  user-select: none;
+
+  &.nav {
+    bottom: 57px;
+  }
+`;
+
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
+
+  overflow-y: auto;
+  overscroll-behavior: none;
+
+  &:last-child {
+    z-index: 3;
+  }
+`;
+
+const ModalWrapper = styled(ScrollFix)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100%;
+`;
+
+type GetPresetFn = (modal?: ModalType) => Preset;
+type Preset = 'nav' | 'regular';
+
+const getPreset: GetPresetFn = (modal) => {
+  switch (modal) {
+    case ModalType.SHOW_MODAL_ACTIONS_MOBILE:
+      return 'nav';
+    default:
+      return 'regular';
+  }
+};
+
+const modalsMap = new Map<ModalType, LazyExoticComponent<ModalPropsType & any>>([
+  [ModalType.SHOW_MODAL_ACTIONS_MOBILE, lazy(() => import('components/modals/ActionsMobileModal'))],
+  [
+    ModalType.SHOW_MODAL_RECEIVE_BITCOIN,
+    lazy(() => import('components/modals/ReceiveBitcoinModal')),
+  ],
+  [
+    ModalType.SHOW_MODAL_TRANSACTION_CONFIRM,
+    lazy(() => import('components/modals/TransactionConfirmModal')),
+  ],
+  [
+    ModalType.SHOW_MODAL_TRANSACTION_DETAILS,
+    lazy(() => import('components/modals/TransactionInfoModals/TransactionDetailsModal')),
+  ],
+  [
+    ModalType.SHOW_MODAL_TRANSACTION_STATUS_SEND,
+    lazy(() => import('components/modals/TransactionInfoModals/TransactionStatusSendModal')),
+  ],
+  [
+    ModalType.SHOW_MODAL_TRANSACTION_STATUS_SWAP,
+    lazy(() => import('components/modals/TransactionInfoModals/TransactionStatusSwapModal')),
+  ],
+  [
+    ModalType.SHOW_MODAL_CLOSE_TOKEN_ACCOUNT,
+    lazy(() => import('components/modals/CloseTokenAccountModal')),
+  ],
+  [
+    ModalType.SHOW_MODAL_PROCEED_USERNAME,
+    lazy(() => import('components/modals/ProceedUsernameModal')),
+  ],
+  [
+    ModalType.SHOW_MODAL_CHOOSE_BUY_TOKEN_MOBILE,
+    lazy(() => import('components/modals/ChooseBuyTokenMobileModal')),
+  ],
+  [
+    ModalType.SHOW_MODAL_SELECT_LIST_MOBILE,
+    lazy(() => import('components/modals/SelectListMobileModal')),
+  ],
+  [ModalType.SHOW_MODAL_ERROR, lazy(() => import('components/modals/ErrorModal'))],
+]);
+
+interface Props {}
+
+export const ModalManager: FC<Props> = observer(() => {
+  const vm = useViewModel(ModalManagerViewModel);
+
+  const handleWrapperClick = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      // handle click only on element
+      if (e.target !== e.currentTarget) {
+        return;
+      }
+
+      vm.closeTopModal();
+    },
+    [vm],
+  );
+
+  const preparedModals = expr(() => {
+    return vm.modals.map((modal) => {
+      const ModalComponent = modalsMap.get(modal.modalType);
+
+      if (!ModalComponent) {
+        return null;
+      }
+
+      return (
+        <Suspense fallback={null} key={modal.modalId}>
+          <ModalContainer>
+            <ModalWrapper onMouseDown={handleWrapperClick}>
+              <ModalComponent
+                {...modal.props}
+                key={modal.modalId}
+                close={(result?: any) => vm.closeModal(modal.modalId, result)}
+              />
+            </ModalWrapper>
+          </ModalContainer>
+        </Suspense>
+      );
+    });
+  });
+
+  if (preparedModals.length === 0) {
+    return null;
+  }
+
+  const preset = getPreset(vm.modals.at(-1)?.modalType);
+  return <Wrapper className={classNames(preset)}>{preparedModals}</Wrapper>;
+});
