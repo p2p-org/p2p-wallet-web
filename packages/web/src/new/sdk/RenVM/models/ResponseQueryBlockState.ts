9bb56318d1db26add011333369794b0880a4393c
Alexey Elizarov <felex-ae@ya.ru>
2022-09-26 15:44:02 +0300

    fix: btc and class-transformer use

--- packages/web/src/new/sdk/RenVM/models/ResponseQueryBlockState.ts
+++ packages/web/src/new/sdk/RenVM/models/ResponseQueryBlockState.ts
@@ -1,91 +1,102 @@
-export class ResponseQueryBlockState {
-  //@ts-ignore
-  state: State;
+import { Type } from 'class-transformer';
 
-  publicKey(mintTokenSymbol: string): string | null {
-    if (mintTokenSymbol === V.btc) {
-      return this.state.v.btc.shards[0]?.pubKey ?? null;
-    }
-    return null;
-  }
+class Outpoint {
+  // @ts-ignore
+  hash: string;
+  // @ts-ignore
+  index: string;
 }
 
-class State {
-  //@ts-ignore
-  t: T;
-  //@ts-ignore
-  v: V;
+class ShardState {
+  @Type(() => Outpoint)
+  // @ts-ignore
+  outpoint: Outpoint;
+  // @ts-ignore
+  pubKeyScript: string;
+  // @ts-ignore
+  value: string;
+}
+
+class Shard {
+  // @ts-ignore
+  pubKey: string;
+  // @ts-ignore
+  shard: string;
+  @Type(() => ShardState)
+  // @ts-ignore
+  state: ShardState;
+}
+
+class Chain {
+  // @ts-ignore
+  burnFee: string;
+  // @ts-ignore
+  chain: string;
+  // @ts-ignore
+  mintFee: string;
+}
+
+class Fees {
+  @Type(() => Chain)
+  // @ts-ignore
+  chains: Chain[];
+}
+
+class BTCValue {
+  @Type(() => Fees)
+  // @ts-ignore
+  fees: Fees;
+  // @ts-ignore
+  gasCap: string;
+  // @ts-ignore
+  gasLimit: string;
+  // @ts-ignore
+  gasPrice: string;
+  // @ts-ignore
+  latestHeight: string;
+  // @ts-ignore
+  minimumAmount: string;
+  @Type(() => Shard)
+  // @ts-ignore
+  shards: Shard[];
+}
+
+class V {
+  @Type(() => BTCValue)
+  // @ts-ignore
+  BTC: BTCValue;
+  // @ios:
+  // let bch, btc, dgb, doge: StructBCH?
+  // let fil, luna: StructFIL?
+  // let system: StructSystem?
+  // let zec: StructBCH?
+
+  get btc(): BTCValue {
+    return this.BTC;
+  }
 }
 
 class T {}
 
-class V {
-  //@ts-ignore
-  btc: BTCValue;
-  // @ios:
-  //            let bch, btc, dgb, doge: StructBCH?
-  //            let fil, luna: StructFIL?
-  //            let system: StructSystem?
-  //            let zec: StructBCH?
+class State {
+  @Type(() => T)
+  // @ts-ignore
+  t: T;
+  @Type(() => V)
+  // @ts-ignore
+  v: V;
+}
 
-  // TODO: CodingKeys
-  static get btc(): string {
-    return 'BTC';
+export class ResponseQueryBlockState {
+  @Type(() => State)
+  // @ts-ignore
+  state: State;
+
+  publicKey(mintTokenSymbol: string): string | null {
+    if (mintTokenSymbol === 'BTC') {
+      debugger;
+      return this.state.v.btc.shards[0]?.pubKey ?? null;
+    }
+    return null;
   }
-}
-
-class BTCValue {
-  //@ts-ignore
-  fees: Fees;
-  //@ts-ignore
-  gasCap: string;
-  //@ts-ignore
-  gasLimit: string;
-  //@ts-ignore
-  gasPrice: string;
-  //@ts-ignore
-  latestHeight: string;
-  //@ts-ignore
-  minimumAmount: string;
-  //@ts-ignore
-  shards: Shard[];
-}
-
-class Fees {
-  //@ts-ignore
-  chains: Chain[];
-}
-
-class Chain {
-  //@ts-ignore
-  burnFee: string;
-  //@ts-ignore
-  chain: string;
-  //@ts-ignore
-  mintFee: string;
-}
-
-class Shard {
-  //@ts-ignore
-  pubKey: string;
-  //@ts-ignore
-  shard: string;
-  //@ts-ignore
-  state: ShardState;
-}
-
-class ShardState {
-  //@ts-ignore
-  outpoint: Outpoint;
-  //@ts-ignore
-  pubKeyScript: string;
-  //@ts-ignore
-  value: string;
-}
-
-class Outpoint {
-  //@ts-ignore
-  hash: string;
-  //@ts-ignore
-  index: string;
 }
