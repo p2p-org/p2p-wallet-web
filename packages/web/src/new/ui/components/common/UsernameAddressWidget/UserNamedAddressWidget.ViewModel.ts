a4380106b2b5540074a4f56db3408d6fbc989c3a
Artem Groznykh <groznykh.dev@gmail.com>
2022-10-13 12:50:34 +0300

    feat(web): notify when coping address username and qr code

--- packages/web/src/new/ui/components/common/UsernameAddressWidget/UserNamedAddressWidget.ViewModel.ts
+++ packages/web/src/new/ui/components/common/UsernameAddressWidget/UserNamedAddressWidget.ViewModel.ts
@@ -20,15 +20,15 @@ export class UserNamedAddressWidgetViewModel extends ViewModel {
     void copyStringToClipboard(
       value,
       () => this._notificationService.info(`${text} copied!`),
-      (error: Error) => console.error(error),
+      (_: Error) => this._notificationService.error(`Can't copy ${text} to clipboard`),
     );
   }
 
   copyQRCode(qrElement: HTMLCanvasElement): void {
     void copyImageToClipboard(
       qrElement,
-      () => this._notificationService.info('QR code Copied!'),
-      (err: Error) => this._notificationService.error(err.message),
+      () => this._notificationService.info('QR Code copied!'),
+      (_: Error) => this._notificationService.error(`Can't copy QR Code to clipboard`),
     );
   }
 }
