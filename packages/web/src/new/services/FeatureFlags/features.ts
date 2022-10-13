7548a039f9ca8396b044c2324dc674fa28b79642
Artem Groznykh <groznykh.dev@gmail.com>
2022-10-13 15:57:36 +0300

    feat(web): add ShowAllSettings feature flag

--- packages/web/src/new/services/FeatureFlags/features.ts
+++ packages/web/src/new/services/FeatureFlags/features.ts
@@ -1,4 +1,5 @@
 // <FeatureName> = '<remote control feature key>'
 export enum Features {
   LeftNavMenuProfile = 'web_feature_LeftNavMenuProfile',
+  ShowAllSettings = 'web_feature_ShowAllSettings',
 }
