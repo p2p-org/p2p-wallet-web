a38e44d579167f65a982adc5b262aa884324bff7
Artem Groznykh <groznykh.dev@gmail.com>
2022-10-25 17:40:36 +0300

    feat(web): swap - tokens changing

--- packages/web/src/new/sdk/Analytics/types/sections/Swap.ts
+++ packages/web/src/new/sdk/Analytics/types/sections/Swap.ts
@@ -1,11 +1,24 @@
 import type { MapActionWithNoParams } from '../common';
 
-type SwapComplexActions = {
-  name: 'Swap_Start_Screen';
-  params: { Last_Screen: string | null };
-};
+type SwapComplexActions =
+  | {
+      name: 'Swap_Start_Screen';
+      params: { Last_Screen: string | null };
+    }
+  | {
+      name: 'Swap_Changing_Token_A';
+      params: {
+        Token_A_Name: string;
+      };
+    }
+  | {
+      name: 'Swap_Changing_Token_B';
+      params: {
+        Token_B_Name: string;
+      };
+    };
 
-type SwapSimpleActionNames = 'Swap_Address_Copied';
+type SwapSimpleActionNames = null;
 
 type SwapSimpleActions = MapActionWithNoParams<SwapSimpleActionNames>;
 
