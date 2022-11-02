5780e6e567b1f3945a02c19c48bcdb60833a1f30
Artem Groznykh <groznykh.dev@gmail.com>
2022-11-02 12:25:45 +0300

    feat(web): send via address

--- packages/web/src/new/utils/RegularExpression.ts
+++ packages/web/src/new/utils/RegularExpression.ts
@@ -2,6 +2,10 @@ export function bitcoinAddress(isTestnet: boolean): RegExp {
   return new RegExp(`^(${isTestnet ? 'tb1|' : ''}bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$`);
 }
 
+export function publickey(): RegExp {
+  return new RegExp('^[1-9A-HJ-NP-Za-km-z]{32,44}$');
+}
+
 export function matches(text: string, regexes: RegExp[]): boolean {
   return regexes.some((regex) => regex.test(text));
 }
