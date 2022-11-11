c329b3e926fc52e941997c59707d5dee4e7746d7
agroznykh <groznykh.dev@gmail.com>
2022-11-11 11:40:56 +0300

    [PWN-5470] send confirm details

--- packages/web/src/new/ui/modals/confirmModals/ConfirmSendModal/ConfirmSendModal.tsx
+++ packages/web/src/new/ui/modals/confirmModals/ConfirmSendModal/ConfirmSendModal.tsx
@@ -15,6 +15,7 @@ import { observer } from 'mobx-react-lite';
 import { Button, Icon } from 'components/ui';
 import { Modal } from 'components/ui/Modal';
 import type { SendViewModel } from 'new/scenes/Main/Send';
+import { FeesView } from 'new/scenes/Main/Send/FeesView';
 import { trackEvent } from 'new/sdk/Analytics';
 import { ButtonCancel } from 'new/ui/components/common/ButtonCancel';
 import { ErrorHint } from 'new/ui/components/common/ErrorHint';
@@ -172,7 +173,7 @@ export const ConfirmSendModal: FC<ConfirmSendModalProps & ModalPropsType> = obse
             <RecipientView viewModel={viewModel} />
           </div>
 
-          {/*<TransactionDetails sendState={sendState} amount={params.amount} />*/}
+          <FeesView viewModel={viewModel} hideAccountCreationFeeSelector={true} />
         </Section>
 
         {isSecretKeyWallet ? (
