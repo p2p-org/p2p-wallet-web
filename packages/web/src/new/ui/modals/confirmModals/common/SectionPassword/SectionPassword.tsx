2c1bd76b39427bb650b2a6f430683ac1121713b7
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-11-11 19:03:15 +0400

    fix: remove hook

--- packages/web/src/new/ui/modals/confirmModals/common/SectionPassword/SectionPassword.tsx
+++ packages/web/src/new/ui/modals/confirmModals/common/SectionPassword/SectionPassword.tsx
@@ -2,7 +2,6 @@ import type { FC } from 'react';
 import { useLayoutEffect, useState } from 'react';
 
 import { styled } from '@linaria/react';
-import { useTryUnlockSeedAndMnemonic } from '@p2p-wallet-web/core';
 import { theme } from '@p2p-wallet-web/ui';
 
 import type { SwapViewModel } from 'new/scenes/Main';
@@ -32,8 +31,6 @@ interface Props {
 
 // TODO: remake it during auth reimplementation
 export const SectionPassword: FC<Props> = ({ onChange, viewModel }) => {
-  const tryUnlockSeedAndMnemonic = useTryUnlockSeedAndMnemonic();
-
   const [password, setPassword] = useState('');
   const [hasError, setHasError] = useState(false);
 
@@ -42,12 +39,9 @@ export const SectionPassword: FC<Props> = ({ onChange, viewModel }) => {
   }, [hasError, viewModel.isMnemonicWallet, onChange, password]);
 
   const validatePassword = async (value: string) => {
-    try {
-      await tryUnlockSeedAndMnemonic(value);
-      setHasError(false);
-    } catch (error) {
-      setHasError(true);
-    }
+    const valid = await viewModel.confirmPassword(value);
+
+    setHasError(!valid);
   };
 
   const handlePasswordChange = (value: string) => {
